0f4e6e1e43bb33701d3e466d1a1c69f7b6b175eb
Mariano Jason <mjasonrc@gmail.com>
2023-06-16 01:07:41 +0200

    Fixed post error

--- app.py
+++ app.py
@@ -23,16 +23,16 @@ IP_POS = 0
 app = Flask(__name__)
 
 
-@app.route("/", defaults={"path": ""})
-@app.route("/<path:path>")
+@app.route("/", defaults={"path": ""}, methods=['GET', 'POST'])
+@app.route("/<path:path>", methods=['GET', 'POST'])
 def catch_all(path):
     global IP_POS
 
-    new_url = f"{IP_LIST[IP_POS]}:{PORT}/{path}"
+    new_url = f"http://{IP_LIST[IP_POS]}:{PORT}/{path}"
     IP_POS = (IP_POS + 1) % len(IP_LIST)
 
     return redirect(new_url, code=302)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(port=8080)

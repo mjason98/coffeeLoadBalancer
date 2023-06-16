a17c24f55e9f24a2b97dc67500af1ca1ca136cdb
Mariano Jason <mjasonrc@gmail.com>
2023-06-16 22:50:49 +0200

    Added health checks

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, redirect
+from flask import Flask, redirect, abort
 from dotenv import load_dotenv
 from utils.get_ips import get_ips
+import requests
 
 load_dotenv()
 
 IP_LIST, PORT = get_ips()
 IP_POS = 0
+MAX_IT = 10 * len(IP_LIST)
 
 app = Flask(__name__)
 
@@ -15,10 +17,23 @@ app = Flask(__name__)
 def catch_all(path):
     global IP_POS
 
-    new_url = f"http://{IP_LIST[IP_POS]}:{PORT}/{path}"
-    IP_POS = (IP_POS + 1) % len(IP_LIST)
+    counter = 0
+    while counter < MAX_IT:
+        url_i = f"http://{IP_LIST[IP_POS]}:{PORT}/v1/healthcheck"
+        response = requests.get(url_i)
 
-    return redirect(new_url, code=302)
+        if response.status_code == 200:
+            break
+        else:
+            counter += 1
+            IP_POS = (IP_POS + 1) % len(IP_LIST)
+
+    if counter < MAX_IT:
+        new_url = f"http://{IP_LIST[IP_POS]}:{PORT}/{path}"
+        IP_POS = (IP_POS + 1) % len(IP_LIST)
+        return redirect(new_url, code=302)
+    else:
+        return abort(400)
 
 
 if __name__ == "__main__":
